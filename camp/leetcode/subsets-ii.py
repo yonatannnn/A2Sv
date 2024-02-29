7ca09bab416d2c963ccb48975d12b6a0999875c2
yonatannnn <133119849+yonatannnn@users.noreply.github.com>
2024-02-28 23:05:17 -0900

    Add solution for Subsets II

---
+++ camp/leetcode/subsets-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        self.ans = set()    
+        def backtrack(start , comb):
+            z = sorted(comb)
+            self.ans.add(tuple(z))
+            for i in range(start , len(nums)):
+                comb.append(nums[i])
+                backtrack(i + 1 , comb)
+                comb.pop()
+        backtrack(0 , [])
+        return list(self.ans)
+        
